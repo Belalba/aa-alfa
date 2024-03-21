0a0a660b620f1e22fd7e931821777953bf2c3f4b
belalbl <belalhossen833777@gmail.com>
2024-03-21 12:31:09 +0600

    finished alfabat

--- alfa-clash.js
+++ alfa-clash.js
@@ -1,4 +1,18 @@
+
+function countinuePlay(){
+   const alfabat = getArandomNumber();
+   console.log(alfabat);
+
+   const currentText = document.getElementById('curret-text');
+   currentText.innerText=alfabat;
+   setBackgroundColor(alfabat);
+}
+
+
+
+
 function play(){
     hidegetelemantid('home-play');
     showgetelemantid('play-ground');
+    countinuePlay();
 }
--- alfa-game.js
+++ alfa-game.js
@@ -9,3 +9,22 @@ function showgetelemantid(elmantid){
     const elmant = document.getElementById(elmantid);
     elmant.classList.remove('hidden')
 }
+
+function setBackgroundColor(elmantid){
+    const elmant = document.getElementById(elmantid);
+    elmant.classList.add('bg-amber-500');
+    
+}
+
+
+function getArandomNumber(){
+    const alfabatstring = ('abcdefghijklmnopqrstvwxyz');
+    const alfabats = alfabatstring.split('');
+    //console.log(alfabats);
+
+
+    const alfabatRandom = Math.random() * 25;
+    const index = Math.round(alfabatRandom);
+    const alfabat = alfabats[index];
+    return alfabat;
+}
